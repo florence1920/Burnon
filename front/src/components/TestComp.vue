4bfbb977eb2d7c3fbb6326569a0bd51971cc22c5
florence1920 <jetaime12121@gmail.com>
2021-11-10 20:13:38 +0900

    test

---
+++ front/src/components/TestComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      test comp
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/views/TestView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <test-comp></test-comp>
+  </div>
+</template>
+
+<script>
+import TestComp from '../components/TestComp.vue'
+export default {
+  components: { TestComp },
+
+}
+</script>
+
+<style>
+
+</style>
