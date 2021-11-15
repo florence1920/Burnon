bc6287a538f0e8f163918c8bf27a5ab66ffad163
yoonhocho <yoonhocho91@gmail.com>
2021-11-15 14:25:26 +0900

    회원 정보 수정 test  css

---
+++ front/src/views/ModifyView.vue
@@ -0,0 +1,15 @@
+<template>
+    <modify-form></modify-form>
+</template>
+
+<script>
+import modifyForm from '../components/modifyForm.vue'
+export default {
+    components: { modifyForm },
+
+}
+</script>
+
+<style>
+
+</style>
