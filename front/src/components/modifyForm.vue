b36ea2d09b5b3dac42c02a699abb5e244c15b4b6
yoonhocho <yoonhocho91@gmail.com>
2021-11-15 13:57:47 +0900

    회원정보 수정 vue 추가

---
+++ front/src/components/modifyForm.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <ul class="info">
+        <li>
+            <p>아이디</p>
+            <p>userId</p>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
